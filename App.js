e44f6dc41aab7ddb64ebd15f1e30aa764d881006
Sani Mridha <sanimridha307@gmail.com>
2021-08-18 16:34:05 +0600

    saniman macOS

---
+++ App.js
@@ -0,0 +1,178 @@
+// import React from 'react';
+// import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+
+// const App = () => {
+//   return (
+//     <View style={styles.container}>
+//       <View
+//         style={{
+//           flexDirection: 'row',
+//           justifyContent: 'space-between',
+//           height: 50,
+//         }}>
+//         <Image
+//           source={{
+//             uri: 'https://zakirsoft.com/frontend/assets/images/logo/1.png',
+//           }}
+//           resizeMode={'contain'}
+//           style={{width: 160, height: 50}}
+//         />
+//         <Image
+//           source={{
+//             uri: 'https://static.thenounproject.com/png/756729-200.png',
+//           }}
+//           resizeMode={'contain'}
+//           style={{width: 30, height: 40, tintColor: 'blue'}}
+//         />
+//       </View>
+//       <ScrollView style={{paddingTop: 15}}>
+//         <Text style={{fontSize: 15, fontWeight: 'bold'}}>About Us</Text>
+//         <Text style={{fontSize: 12, top: 5, left: 15}}>
+//           Zakir Soft is a Software company and Software Development Training
+//           Institute. We develop Softwares for your business and we also provide
+//           quality software training in Adabor Since 2020. We have passionate
+//           teams like Full Stack Laravel Developer, Android Developer, UI/UX
+//           Designer. We have management system softwares for the local market. we
+//           also have eCommerce systems for your online business. We started this
+//           business to provide good softwares and quality of training at a better
+//           price. Zakirsoft specialize in working directly with other
+//           organizations to create sustainable partnerships. In fact, we recently
+//           helped student's to give them a correct path to become a web
+//           developer.Zakir Soft is a Software company and Software Development
+//           Training Institute. We develop Softwares for your business and we also
+//           provide quality software training in Adabor Since 2020. We have
+//           passionate teams like Full Stack Laravel Developer, Android Developer,
+//           UI/UX Designer. We have management system softwares for the local
+//           market. we also have eCommerce systems for your online business. We
+//           started this business to provide good softwares and quality of
+//           training at a better price. Zakirsoft specialize in working directly
+//           with other organizations to create sustainable partnerships. In fact,
+//           we recently helped student's to give them a correct path to become a
+//           web developer.
+//         </Text>
+//         <Text
+//           style={{
+//             fontSize: 15,
+//             fontWeight: 'bold',
+//             color: 'green',
+//             paddingVertical: 15,
+//           }}>
+//           Codeshikhi
+//         </Text>
+//         <Text style={{fontSize: 12, left: 15}}>
+//           Zakir Soft is a Software company and Software Development Training
+//           Institute. We develop Softwares for your business and we also provide
+//           quality software training in Adabor Since 2020. We have passionate
+//           teams like Full Stack Laravel Developer, Android Developer, UI/UX
+//           Designer. We have management system softwares for the local market. we
+//           also have eCommerce systems for your online business. We started this
+//           business to provide good softwares and quality of training at a better
+//           price. Zakirsoft specialize in working directly with other
+//           organizations to create sustainable partnerships. In fact, we recently
+//           helped student's to give them a correct path to become a web
+//           developer.Zakir Soft is a Software company and Software Development
+//           Training Institute. We develop Softwares for your business and we also
+//           provide quality software training in Adabor Since 2020. We have
+//           passionate teams like Full Stack Laravel Developer, Android Developer,
+//           UI/UX Designer. We have management system softwares for the local
+//           market. we also have eCommerce systems for your online business. We
+//           started this business to provide good softwares and quality of
+//           training at a better price. Zakirsoft specialize in working directly
+//           with other organizations to create sustainable partnerships. In fact,
+//           we recently helped student's to give them a correct path to become a
+//           web developer.
+//         </Text>
+//         <Text
+//           style={{
+//             fontSize: 15,
+//             fontWeight: 'bold',
+//             color: 'blue',
+//             paddingVertical: 15,
+//           }}>
+//           Echo Templates
+//         </Text>
+//         <Text style={{fontSize: 12, left: 15}}>
+//           Zakir Soft is a Software company and Software Development Training
+//           Institute. We develop Softwares for your business and we also provide
+//           quality software training in Adabor Since 2020. We have passionate
+//           teams like Full Stack Laravel Developer, Android Developer, UI/UX
+//           Designer. We have management system softwares for the local market. we
+//           also have eCommerce systems for your online business. We started this
+//           business to provide good softwares and quality of training at a better
+//           price. Zakirsoft specialize in working directly with other
+//           organizations to create sustainable partnerships. In fact, we recently
+//           helped student's to give them a correct path to become a web
+//           developer.Zakir Soft is a Software company and Software Development
+//           Training Institute. We develop Softwares for your business and we also
+//           provide quality software training in Adabor Since 2020. We have
+//           passionate teams like Full Stack Laravel Developer, Android Developer,
+//           UI/UX Designer. We have management system softwares for the local
+//           market. we also have eCommerce systems for your online business. We
+//           started this business to provide good softwares and quality of
+//           training at a better price. Zakirsoft specialize in working directly
+//           with other organizations to create sustainable partnerships. In fact,
+//           we recently helped student's to give them a correct path to become a
+//           web developer.
+//         </Text>
+//         <Text
+//           style={{
+//             fontSize: 15,
+//             fontWeight: 'bold',
+//             color: 'coral',
+//             paddingVertical: 15,
+//           }}>
+//           Template Cookie
+//         </Text>
+//         <Text style={{fontSize: 12, left: 15}}>
+//           Zakir Soft is a Software company and Software Development Training
+//           Institute. We develop Softwares for your business and we also provide
+//           quality software training in Adabor Since 2020. We have passionate
+//           teams like Full Stack Laravel Developer, Android Developer, UI/UX
+//           Designer. We have management system softwares for the local market. we
+//           also have eCommerce systems for your online business. We started this
+//           business to provide good softwares and quality of training at a better
+//           price. Zakirsoft specialize in working directly with other
+//           organizations to create sustainable partnerships. In fact, we recently
+//           helped student's to give them a correct path to become a web
+//           developer.Zakir Soft is a Software company and Software Development
+//           Training Institute. We develop Softwares for your business and we also
+//           provide quality software training in Adabor Since 2020. We have
+//           passionate teams like Full Stack Laravel Developer, Android Developer,
+//           UI/UX Designer. We have management system softwares for the local
+//           market. we also have eCommerce systems for your online business. We
+//           started this business to provide good softwares and quality of
+//           training at a better price. Zakirsoft specialize in working directly
+//           with other organizations to create sustainable partnerships. In fact,
+//           we recently helped student's to give them a correct path to become a
+//           web developer.
+//         </Text>
+//       </ScrollView>
+//     </View>
+//   );
+// };
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     // flex: 1,
+//     justifyContent: 'center',
+//     // alignItems: 'center',
+//     paddingHorizontal: 20,
+//   },
+// });
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hey there this is saniman👋</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
